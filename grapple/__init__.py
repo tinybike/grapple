274c7ccb53d303651a71ad8822eb5c1cf2044405
Jack Peterson <jack@tinybike.net>
2014-08-29 16:24:05 -0700

    readme text

--- grapple/__init__.py
+++ grapple/__init__.py
@@ -477,8 +477,7 @@ def main(argv=None):
         'setup': True,
         'precision': 28,
         'socket_url': 'ws://127.0.0.1:6006/',
-        # 'halt': 152370,  # Genesis ledger
-        'halt': 8141600,
+        'halt': 152370,  # Genesis ledger
     }
     for opt, arg in opts:
         if opt in ('-h', '--help'):
